fae65bbe6b5278c26cdf6357dd6e14c0816bab48
throvn <Faceformer1>
2019-06-03 18:59:58 +0200

    Fixed import structure

---
+++ chalk.py
@@ -0,0 +1,17 @@
+def green(text):
+    return '\033[92m' + text + '\033[0m'
+    
+def blue(text):
+    return '\033[94m' + text + '\033[0m'
+
+def yellow(text):
+    return '\033[93m' + text + '\033[0m'
+
+def red(text):
+    return '\033[91m' + text + '\033[0m'
+
+def bold(text):
+    return '\033[1m' + text + '\033[0m'
+
+def underline(text):
+    return '\033[4m' + text + '\033[0m'
