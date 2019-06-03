9f3c3ac207fddda7ea82687ea7927457eb50667d
throvn <Faceformer1>
2019-06-03 19:56:07 +0200

    Updated Readme.md and added more colors

--- chalk.py
+++ chalk.py
@@ -10,8 +10,26 @@ def yellow(text):
 def red(text):
     return '\033[91m' + text + '\033[0m'
 
+def white(text):
+    return '\u001b[37m' + text + '\033[0m'
+
+def black(text):
+    return '\u001b[30m' + text + '\033[0m'
+
+def magenta(text):
+    return '\u001b[35m' + text + '\033[0m'
+
+def cyan(text):
+    return '\u001b[36m' + text + '\033[0m'
+
+    
+# Decorations
+
 def bold(text):
     return '\033[1m' + text + '\033[0m'
 
 def underline(text):
     return '\033[4m' + text + '\033[0m'
+
+def reversed(text):
+    return '\033[7m' + text + '\033[0m'
