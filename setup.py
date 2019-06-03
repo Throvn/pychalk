571d7a6112cac6f579509b35f9d4f617ac411ecc
throvn <Faceformer1>
2019-06-03 18:56:15 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='PyChalk',
+    url='https://github.com/Throvn/pychalk',
+    author='Louis Stanko',
+    author_email='louis.stanko1@gmail.com',
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Colors for your Terminal.',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
